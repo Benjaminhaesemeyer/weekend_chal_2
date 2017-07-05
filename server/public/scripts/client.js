9a3c8d87fbcc19b2414e7c517cd52a6b21f14a2e
Benjaminhaesemeyer <benjaminhaesemeyer@gmail.com>
2017-07-04 21:40:45 -0500

    server and client built

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,34 @@
 console.log('JS Sourced')
+
+$(document).ready(function(){
+  //create calculate click function
+  $('#calculate').on('click',function(){
+    console.log('calc click');
+//number inputs
+var x = $('#num1').val();
+var y = $('#num2').val();
+
+var operator = $('#operator').val();
+
+var valuesObject = {
+  x: x,
+  y: y,
+  type: operator
+};
+    console.log(valuesObject);
+
+ $.ajax({
+      type: 'POST',
+      url: '/outcome',
+      data: valuesObject,
+      sucess: function(result){
+        console.log('success, result = ', result);
+        display(result);
+      },
+    });
+  });
+  function display(answerObject){
+    console.log(answerObject);
+    $('#outcome').append(answerObject.answer);
+  }
+});
--- server/server.js
+++ server/server.js
@@ -4,13 +4,50 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var port = 5000;
 
-app.use(bodyParser.urlencoded({extened: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/*", function(req, res){
   var file = req.params[0] || "views/index.html";
   res.sendFile(path.join(__dirname, "public", file));
 });
 
+app.post('/outcome', function(req,res){
+  console.log('in post route');
+  console.log('req.body in post = ', req.body);
+  calcResults(req.body);
+  res.sendStatus(200);
+});
+// created a calculator function
+calcResults = function (calcButton){
+  console.log('calc results working');
+
+  var x = Number(calcButton.x);
+  var y = Number(calcButton.y);
+  var operator = calcButton.type;
+
+//declare answer variable
+  var answer;
+//switch statement for choosing operator
+  switch (operator) {
+    case 'add':
+      answer = x + y;
+      break;
+    case 'subtract':
+      answer = x - y;
+      break;
+    case 'multiply':
+      answer = x * y;
+      break;
+    case 'divide':
+      answer = x / y;
+      break;
+    default:
+      console.log('error');
+      answer = 'operator error';
+  }
+  return answer;
+
+};
 
 app.listen(port, function() {
   console.log('listening on port', port);
